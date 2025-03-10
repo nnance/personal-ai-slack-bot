88792961918e9a9da55d89d18117e8b608ab874d
Nick Nance <nance.nick@gmail.com>
2025-03-09 21:25:17 -0500

    initial ai sdk integration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+import "dotenv/config";
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+
+generateText({
+  model: openai("gpt-4o"),
+  system: `You are a Slack bot assistant Keep your responses concise and to the point.
+    - Do not tag users.
+    - Current date is: ${new Date().toISOString().split("T")[0]}
+    - Make sure to ALWAYS include sources in your final response if you use web search. Put sources inline if possible.`,
+  prompt: "What is the capital of France?",
+}).then(({ text }) => {
+  console.log(text);
+});
